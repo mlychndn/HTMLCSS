aea863f3fd6a99db5be69c4df83c96a6ca4ae37a
Malay <mlychndn@gmail.com>
2023-04-05 02:02:47 +0530

    section 2 started

---
+++ section-2/index.js
@@ -0,0 +1,42 @@
+// setTimeout(() => {
+//   for (let i = 0; i <= 6; i++) {
+//     console.log(i);
+//   }
+// }, 1000);
+
+// for (let i = 0; i <= 6; i++) {
+//   setTimeout(() => {
+//     console.log(i);
+//   }, 1000);
+// }
+
+//
+const a = [10, "Tithi", [10, 20], { a: 1, b: 1 }];
+
+// for (let i = 0; i <= a.length - 1; i++) {
+//   console.log(typeof a[i]);
+// }
+
+// let i = 0;
+// while (i < a.length) {
+//   console.log(a[i]);
+//   i++;
+// }
+
+// a.push(10);
+// a.push(20);
+// a.push(30);
+// a.push(40);
+// a.push(50);
+
+// a.pop();
+// a.pop();
+// console.log(a);
+// console.log(a.length);
+
+// console.log(a);
+
+// let b = 10;
+// console.log(typeof b);
+// console.log(typeof a);
+// console.log(typeof "tithi");
