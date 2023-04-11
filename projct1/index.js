34bfc22978b0ae0f5502d9513f62506bbab6e5ff
Malay <mlychndn@gmail.com>
2023-04-11 15:38:59 +0530

    audio tape added

--- projct1/index.js
+++ projct1/index.js
@@ -11,4 +11,12 @@ setInterval(() => {
   hourHand.style.transform = `rotate(${30 * hourTime + minTime / 2}deg)`;
   minuteHand.style.transform = `rotate(${6 * minTime}deg)`;
   secondHand.style.transform = `rotate(${6 * secTime}deg)`;
+
+  // console.log(`sound-bell-${hourTime}`);
+
+  const audioEl = document.querySelector("audio");
+  console.log(audioEl);
+  // const audioE2 = document.querySelector(`.sound-bell-${hourTime}`);
+  // audioEl.play();
+  // console.log(audioE2);
 }, 1000);
